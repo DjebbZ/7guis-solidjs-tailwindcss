44d898fe2af57ef2615f85fe2080cc02ca1a31d0
DjebbZ <khalid.jebbari@gmail.com>
2022-06-24 16:21:34 +0200

    More styles + Task component for... styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import { Component } from "solid-js";
 import Counter from "./Task1-Counter";
 
+const Task: Component = ({ children }) => {
+  return (
+    <section class="m-2 border-b-gray-300 border-dashed border-b-1 pb-8">
+      {children}
+    </section>
+  );
+};
+
 const App: Component = () => {
-  return <Counter />;
+  return (
+    <>
+      <Task>
+        <Counter />
+      </Task>
+    </>
+  );
 };
 
 export default App;
